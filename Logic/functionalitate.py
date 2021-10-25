82c7e7dc4ed902869a0909b37fad25769c020ec9
alexiahudea2003 <91561462+alexiahudea2003@users.noreply.github.com>
2021-10-25 17:11:57 +0300

    Update functionalitate.py

--- Logic/functionalitate.py
+++ Logic/functionalitate.py
@@ -1,13 +1,1 @@
-from Domain.inventar import get_id,get_nume,get_descriere,get_pret,get_locatie,gestionare_obiect
-def mutare_obiect(locatie_noua, lista):
-    get_locati = locatie_noua
-    for obiect in lista:
-        obiect_mutat = gestionare_obiect(
-            get_id(obiect),
-            get_nume(obiect),
-            get_descriere(obiect),
-            get_pret(obiect),
-            get_locatie(obiect),
-        )
-        lista.append(obiect_mutat)
-    return lista
+
